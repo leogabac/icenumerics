b65f0fda33b290a6bc9d0b9731073e20a42c596b
crodriga <carolrodriguezgallo@ub.edu>
2020-05-12 14:36:30 +0200

    Add new square.py file
    Add vertex closed, and closed spins boundary conditions to square.py

--- icenumerics/geometry/square.py
+++ icenumerics/geometry/square.py
@@ -1,4 +1,5 @@
 import numpy as np
+import scipy.spatial as spa
 
 def SquareSpinIceConcatenateCenters(VSpinsX,VSpinsY,HSpinsX,HSpinsY,Lattice,Boundary):
     """ Concatenate Vertical and Horizontal spin arrays """
@@ -87,6 +88,81 @@ def square_spin_ice_geometry(sx,sy,lattice,border):
         t = np.array([0,90])/180*np.pi
         unit_cell_center = lattice/2*np.array(
             [np.cos(t),np.sin(t),np.zeros(len(t))]).transpose()
+        unit_cell_direction = lattice*np.array(
+            [np.cos(t),np.sin(t),np.zeros(len(t))]).transpose()
+   
+    elif border == "closed spin":
+        
+        t = np.array([0,90])/180*np.pi
+        
+        unit_cell_center = np.array(
+            [[lattice/2,0,0],
+             [0,lattice/2,0],
+             [lattice,lattice/2,0],
+             [lattice/2,lattice,0]])
+      
+      
+        
+        unit_cell_direction = lattice*np.array(
+            [[1,0,0],[0,1,0],[0,1,0],[1,0,0]])
+    
+    elif border == "closed vertex":
+        
+        t = np.array([0,90])/180*np.pi
+        
+        unit_cell_center = np.array(
+            [[lattice/2,0,0],
+             [0,lattice/2,0],
+             [lattice,lattice/2,0],
+             [lattice/2,lattice,0]])
+      
+      
+        
+        unit_cell_direction = lattice*np.array(
+            [[0,-1,0],[1,0,0],[1,0,0],[0,1,0]])
+        
+        
+    else: 
+        raise(ValueError(border+" is not a supporteed border type."))
+        
+    space = np.meshgrid(
+        np.arange(0,sx)*lattice,
+        np.arange(0,sy)*lattice,
+        np.arange(1))
+        
+    n = np.array([1,1,1])
+    R = np.array([s.flatten()*n[i] for i,s in enumerate(space)])
+    
+    centers = np.concatenate([R.transpose()+c for c in unit_cell_center])
+    directions = np.concatenate([np.zeros(np.shape(R.transpose()))+c for c in unit_cell_direction])
+    
+    tree = spa.cKDTree(centers)
+    remove = [p[1] for p in tree.query_pairs(1e-10)]
+    
+    mask = np.ones(len(centers),dtype=bool)
+    mask[remove] = False
+    
+    centers = centers[mask]
+    directions = directions[mask]
+
+    
+    return centers, directions
+    
+def square_spin_ice_geometry3D(sx,sy,lattice,h,border):
+    """ 
+    This function calculates the positions and directions of the spins in a square spin ice system. 
+    
+    It does so by creating two meshes, 
+    where one mesh defines the position of the horizontal spins, 
+    and the other defines the position of the vertical spins.
+    Then a subroutine concatenates the arrays.
+    """
+    
+    if border == "periodic":
+    
+        t = np.array([0,90])/180*np.pi
+        unit_cell_center = np.array(
+            [np.cos(t)*lattice/2,np.sin(t)*lattice/2,np.sin(t)*h]).transpose()
         unit_cell_direction = lattice*np.array(
             [np.cos(t),np.sin(t),np.zeros(len(t))]).transpose()
 
